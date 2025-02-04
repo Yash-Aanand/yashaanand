7ef3d8921f912fbc6a9001220e6d0400a3d3fd8e
~Yashh <61193710+Yash-Aanand@users.noreply.github.com>
2025-02-04 18:39:41 -0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/yashaanand/", // Replace with your repository name
+  base: "/", // Replace with your repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
